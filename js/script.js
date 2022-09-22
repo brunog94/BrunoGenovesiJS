f3159cccbc5cd5e12a6dcba42b1301dfa616e3b2
Bruno Genovesi <bruno.genovesi@hotmail.com>
2022-09-22 19:42:18 -0300

    Entrega js

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
-/* let variable;
-let Variable;
-let vAriable;
+let ogCush= 'og cush';
+let sepaWidow= 'black widow'
+let sePa = prompt('elegi tu sepa');
 
-distingue las may y guiones bajos */
-
+if(sePa==ogCush){
+    console.log('el precio es 1200')
+    alert('el precio es 1200')
+}else if(sePa==sepaWidow){
+    alert('el precio es 1400')
+}else{
+    alert('no elegiste una opcion correcta')
+}
+alert('aniadido al carrito')
