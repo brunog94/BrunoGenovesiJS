0a0a7cbf1427e9f2fdbd721328c0bb8c61657ed9
Bruno Genovesi <bruno.genovesi@hotmail.com>
2022-09-27 09:22:11 -0300

    Cliclos y COndicionales

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
+
+
+for(let i = 1; i <=2;i++){
+    
 let ogCush= 'og cush';
 let sepaWidow= 'black widow'
 let sepaDream = ' blue dream'
 let sepaJack='jack here'
-let sePa = prompt('elegi tu sepa');
+let sepa = prompt('elegi tu sepa');
 
-if(sePa==ogCush){
+if(sepa==ogCush){
 
     alert('el precio es 1200')
-}else if(sePa==sepaWidow){
+    alert('sumado al carrito')
+}else if(sepa==sepaWidow){
     alert('el precio es 1400')
-}else if(sePa==sepaDream){
+    alert('sumado al carrito')
+}else if(sepa==sepaDream){
     alert('el precio es 1500')
-}else if (sePa==sepaJack){
+    alert('sumado al carrito')
+}else if (sepa==sepaJack){
     alert('el precio es 1100')
+    alert('sumado al carrito')
 }else{
     alert('no elegiste una opcion correcta')
 }
-console.log(sePa)
-
-for(let i = 1; i <=2;i++){
-   prompt('elegi otra sepa')
-    if ( i = sePa){
-        alert('elegiste la'+' '+ i +'da'+" "+"sepa")
-    }
+console.log(sepa)
 }
     
