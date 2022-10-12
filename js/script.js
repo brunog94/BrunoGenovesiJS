740eec945ebd2243e399a4d584c04cac08d97f1c
Bruno Genovesi <bruno.genovesi@hotmail.com>
2022-10-11 22:06:33 -0300

    entrega 2 JAVA SCRIPT

--- js/script.js
+++ js/script.js
@@ -1,29 +1,36 @@
+// funcion ciclo
+var array = [
+    {value: "og_cush", text: "Og Cush"},
+    {value: "black_widow", text: "Black Widow"},
+    {value: "blue_dream", text: "Blue Dream"},
+    {value: "jack_herer", text: "Jack Herer"},
+    {value: "gorilla_glue", text: "Gorilla Glue"}
+];
 
+var select = document.getElementById('sepas'),
+    i = 0,
+    il = array.length;
 
-for(let i = 1; i <=2;i++){
-    
-let ogCush= 'og cush';
-let sepaWidow= 'black widow'
-let sepaDream = ' blue dream'
-let sepaJack='jack here'
-let sepa = prompt('elegi tu sepa');
-
-if(sepa==ogCush){
-
-    alert('el precio es 1200')
-    alert('sumado al carrito')
-}else if(sepa==sepaWidow){
-    alert('el precio es 1400')
-    alert('sumado al carrito')
-}else if(sepa==sepaDream){
-    alert('el precio es 1500')
-    alert('sumado al carrito')
-}else if (sepa==sepaJack){
-    alert('el precio es 1100')
-    alert('sumado al carrito')
-}else{
-    alert('no elegiste una opcion correcta')
+for (i; i < il; i ++) {
+  option = document.createElement('option');
+  option.setAttribute('value', array[i].value);
+  option.appendChild(document.createTextNode(array[i].text));
+  select.appendChild(option);
 }
-console.log(sepa)
-}
-    
+
+// funcion condicional
+var precio =  document.getElementById('precio');
+
+document.getElementById('sepas').addEventListener('change', function() {
+  var sepa =  this.value;
+
+  if (sepa == 'og_cush'){
+    precio.value = '1200';
+  }else if (sepa == 'black_widow'){
+    precio.value = '1400';
+  }else if (sepa == 'blue_dream'){
+    precio.value = '1500';
+  }else {
+    precio.value = '1100';
+  }
+});
